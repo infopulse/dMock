7252b0ca30ec29ebe995e01c55cce4a68f0cbd2b
WildDemonUa <pleskach.oleh@lll.kpi.ua>
2024-04-24 18:42:22 +0300

    Added drud operators placeholders

---
+++ dmock/api/operator.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from typing import Dict
+
+app = FastAPI()
+
+def create_mock_static():
+    ...
+
+def delete_mock_static():
+    ...
+
+def get_mock_static():
+    ...
+
+def update_mock_static():
+    ...
